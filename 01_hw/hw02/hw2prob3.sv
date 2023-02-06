8cf2205b211837350e4ce45b2864d5fbf6b95565
Xinye Wang <xinyew@ece013.ece.local.cmu.edu>
2023-02-06 16:15:37 -0500

    2

--- 01_hw/hw02/hw2prob3.sv
+++ 01_hw/hw02/hw2prob3.sv
@@ -2,10 +2,11 @@ module hw2prob3
   (input logic a, b, c, d,
   output logic l_assign, l_assign_min, l_case, l_casez);
 
-  assign l_assign = {a, b, c, d} == 4'd0 || {a, b, c, d} == 4'd2 || {a, b, c, d} == 4'd4
-          || {a, b, c, d} == 4'd6 || {a, b, c, d} == 4'd7 || {a, b, d, d} == 4'd10
-          || {a, b, c, d} == 4'd13 || {a, b, c, d} == 4'd5 || {a, b, c, d} == 4'd8
-          || {a, b, c, d} == 4'd15;
+  assign l_assign = {a, b, c, d} == 4'd0 || {a, b, c, d} == 4'd2
+                 || {a, b, c, d} == 4'd4 || {a, b, c, d} == 4'd6
+                 || {a, b, c, d} == 4'd7 || {a, b, d, d} == 4'd10
+                 || {a, b, c, d} == 4'd13 || {a, b, c, d} == 4'd5
+                 || {a, b, c, d} == 4'd8 || {a, b, c, d} == 4'd15;
 
   assign l_assign_min = {b, d} == 2'b11 || {a, d} == 2'b00 || {a, b, d} == 3'b100;
 
@@ -20,7 +21,7 @@ module hw2prob3
       4'd13: l_case = 1;
       default: l_case = 0;
     endcase
-  
+
   always_comb
     unique casez ({a, b, c, d})
       4'b?1?1: l_casez = 1;
@@ -47,7 +48,8 @@ module hw2prob3_test;
   initial begin
     $monitor($time,,
       "a = %b, b = %b, c = %b, d = %b, l1 = %b, l2 = %b, l3 = %b, l4 = %b",
-      vector[3], vector[2], vector[1], vector[0], l_assign, l_assign_min, l_case, l_casez);
+      vector[3], vector[2], vector[1], vector[0],
+      l_assign, l_assign_min, l_case, l_casez);
     for (vector = 4'b0000; vector < 4'b1111; vector++)
       #1;
     #1 $finish;
